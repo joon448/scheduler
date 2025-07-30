665f18f61c2deeb4c3b0157b953354bfe65decfe
joon448 <joonjdy448@gmail.com>
2025-07-30 17:20:08 +0900

    Feat: Lv1: ScheduleRequestDto 클래스 생성 (DTO)

---
+++ src/main/java/org/example/scheduler/dto/ScheduleRequestDto.java
@@ -0,0 +1,11 @@
+package org.example.scheduler.dto;
+
+import lombok.Getter;
+
+@Getter
+public class ScheduleRequestDto {
+    private String name;
+    private String password;
+    private String title;
+    private String content;
+}
