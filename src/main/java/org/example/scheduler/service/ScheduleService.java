38228fd97cb7e25b7a6414d36f73845b8a8822a6
joon448 <joonjdy448@gmail.com>
2025-07-30 17:21:20 +0900

    Feat: Lv1: ScheduleService 클래스 생성 (Service)

---
+++ src/main/java/org/example/scheduler/service/ScheduleService.java
@@ -0,0 +1,32 @@
+package org.example.scheduler.service;
+
+import lombok.RequiredArgsConstructor;
+import org.example.scheduler.entity.Schedule;
+import org.example.scheduler.repository.ScheduleRepository;
+import org.example.scheduler.dto.ScheduleRequestDto;
+import org.example.scheduler.dto.ScheduleResponseDto;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class ScheduleService {
+    private final ScheduleRepository scheduleRepository;
+
+    @Transactional
+    public ScheduleResponseDto save(ScheduleRequestDto scheduleRequestDto){
+        Schedule schedule = new Schedule(scheduleRequestDto.getName(), scheduleRequestDto.getPassword(), scheduleRequestDto.getTitle(), scheduleRequestDto.getContent());
+        scheduleRepository.save(schedule);
+        return new ScheduleResponseDto(schedule);
+    }
+
+    @Transactional(readOnly = true)
+    public List<ScheduleResponseDto> findAll(){
+        return scheduleRepository.findAll().stream().map(ScheduleResponseDto::new).collect(Collectors.toList());
+    }
+
+
+}
