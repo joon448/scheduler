c8b2d168a703b21421bb6b80b4e1c95415a47045
joon448 <joonjdy448@gmail.com>
2025-07-30 17:19:29 +0900

    Feat: Lv1: Schedule 클래스 생성 (Entity)

---
+++ src/main/java/org/example/scheduler/entity/Schedule.java
@@ -0,0 +1,34 @@
+package org.example.scheduler.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Schedule extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 20)
+    private String name;
+
+    @Column(nullable = false, length = 50)
+    private String password;
+
+    @Column(nullable = false, length = 30)
+    private String title;
+
+    @Column(nullable = false, length = 200)
+    private String content;
+
+    public Schedule(String name, String password, String title, String content) {
+        this.name = name;
+        this.password = password;
+        this.title = title;
+        this.content = content;
+    }
+}
